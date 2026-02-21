c9dc2977ca2668f8438dc631128919b71b621adf
Equality <hariasharii94@gmail.com>
2026-02-22 03:57:25 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-STAFF_ROLE_NAME = "🔰 Admin Store"
+STAFF_ROLE_NAME = "Admin Store"
 DANA_NUMBER = "081266778093"
 BCA_NUMBER = "8565330655"
 RATE = 85
